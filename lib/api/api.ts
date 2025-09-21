2c810b858b7366d3f6c629c6d2f75916abc8c41c
AnnaYarmolenko <93723763+AnnaYarmolenko@users.noreply.github.com>
2025-09-21 09:44:39 +0300

    Fixed base URL

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 // Створюємо інстанс axios
 export const nextServer = axios.create({
-  // baseURL: process.env.NEXT_PUBLIC_API_URL+'/api',
-  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL+'/api',
+  // baseURL: 'http://localhost:3000/api',
   withCredentials: true, // дозволяє axios працювати з cookie
 });
 
